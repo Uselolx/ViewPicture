f8273dfabf7abf58387552841021ff3767020cdd
Никита Медонов <medon11291@yandex.ru>
2021-04-20 00:44:17 +0300

    Добавьте файлы проекта.

---
+++ WpfAppForCapgemini/Model/MyImage.cs
@@ -0,0 +1,52 @@
+﻿namespace WpfAppForCapgemini
+{
+    public class MyImage: ObserableObject
+    {
+        private string filePath;
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set
+            {
+                if (filePath != value)
+                {
+                    filePath = value;
+                    OnPropertyChanged(nameof(FilePath));
+                }
+            }
+        }
+
+
+        private string fileName;
+
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                if (fileName != value)
+                {
+                    fileName = value;
+                    OnPropertyChanged(nameof(FileName));
+                }
+            }
+        }
+
+
+        private int size;
+
+        public int Size
+        {
+            get { return size; }
+            set
+            {
+                if (size != value)
+                {
+                    size = value;
+                    OnPropertyChanged(nameof(Size));
+                }
+            }
+        }
+    }
+}
---
+++ WpfAppForCapgemini/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using WpfApp1.Helpers;
+
+
+namespace WpfAppForCapgemini
+{
+    internal class MainViewModel : ObserableObject
+    {
+        public ObservableCollection<MyImage> Images { set; get; }
+        public MainViewModel()
+        {
+            Images = new ObservableCollection<MyImage>();
+        }
+        private MyImage selectedImage;
+
+        public MyImage SelectedImage
+        {
+            get { return selectedImage; }
+            set
+            {
+                if (selectedImage != value)
+                {
+                    selectedImage = value;
+                    OnPropertyChanged(nameof(SelectedImage));
+                }
+            }
+        }
+
+
+        RelayCommand addCommand;
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(
+                    (obj) =>
+                    {
+                        var dialog = new CommonOpenFileDialog();
+                        dialog.IsFolderPicker = true;
+                        CommonFileDialogResult result = dialog.ShowDialog();
+                        if (result == CommonFileDialogResult.Ok)
+                        {
+                            DirectoryInfo info = new DirectoryInfo(dialog.FileName);
+                            foreach (var item in info.GetFiles("*.jpg"))
+                            {
+                                MyImage img = new MyImage();
+                                FileInfo fileinfo = new FileInfo(item.FullName);
+                                img.FilePath = fileinfo.FullName;
+                                img.FileName = fileinfo.Name;
+                                Images.Add(img);
+                            }
+                        }
+                    }));
+            }
+
+        }
+    }
+}
